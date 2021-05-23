b123cf4e50f0be46a5a120804bff70c0c498020f
jgabriel1 <josegabriel@det.ufc.br>
2021-05-23 16:13:39 -0300

    Added search logic.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,35 @@
+import { useRef, useState } from 'react';
+
+import { CarSearchClient, Card } from '../services/cardData';
+import useDebounce from '../utils/hooks/useDebounce';
+
 export default function Home() {
-  return <h1>Hello world</h1>;
+  const { current: client } = useRef(new CarSearchClient());
+
+  const [cards, setCards] = useState<Card[]>([]);
+
+  const searchCard = useDebounce(
+    async (text: string) => {
+      const possibleCards = await client.searchCardName(text);
+
+      setCards(possibleCards);
+    },
+    [],
+    1000
+  );
+
+  return (
+    <div>
+      <input type="text" onChange={e => searchCard(e.target.value)} />
+
+      <ul>
+        {cards.map(card => (
+          <li key={card.id}>
+            <span>{card.mana_cost}</span>
+            <span>{card.name}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
