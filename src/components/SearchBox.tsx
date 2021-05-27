37b40ac189eed1563a002a8f0b07890049ce7010
jgabriel1 <josegabriel@det.ufc.br>
2021-05-27 20:25:12 -0300

    Created logic for fetching card data through react-query.

--- src/components/SearchBox.tsx
+++ src/components/SearchBox.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, FormEventHandler } from 'react';
+import { FormEventHandler, FunctionComponent, ChangeEventHandler } from 'react';
 import { Input, InputProps } from '@chakra-ui/input';
 import {
   HStack,
@@ -10,21 +10,68 @@ import {
   MenuButton,
   VStack,
 } from '@chakra-ui/react';
+import { CARD_DATA, POSSIBLE_CARD_NAMES } from '../data/keys';
+import { useMutation } from 'react-query';
+import {
+  CardData,
+  getCardDataFromName,
+  getCardNameAutoComplete,
+} from '../services/cardData';
+import { withDebounce } from '../utils/withDebounce';
 
-interface SearchBoxProps extends InputProps {
-  onSubmitCard: FormEventHandler;
-  possibleCards: string[];
-}
+type SearchBoxProps = {
+  onSubmitCard: (card: CardData) => void;
+};
+
+type ChangeInputEventHandler = ChangeEventHandler<HTMLInputElement>;
 
 export const SearchBox: FunctionComponent<SearchBoxProps> = ({
   onSubmitCard,
-  possibleCards,
-  onChange,
 }) => {
+  const { mutate: mutatePossibleCards, data: possibleCards } = useMutation({
+    mutationKey: POSSIBLE_CARD_NAMES,
+    mutationFn: async (q: string) => {
+      console.log('fetched possibles');
+
+      return await getCardNameAutoComplete(q);
+    },
+  });
+
+  const {
+    mutate: mutateSelectedCard,
+    data: selectedCard,
+    reset: resetSelectedCard,
+  } = useMutation({
+    mutationKey: CARD_DATA,
+    mutationFn: async (q: string) => {
+      console.log('fetched');
+
+      return await getCardDataFromName(q);
+    },
+    onSettled: (data, _, q) => {
+      if (data === null) mutatePossibleCards(q);
+    },
+  });
+
+  const handleSubmitCard: FormEventHandler = event => {
+    event.preventDefault();
+
+    if (selectedCard) {
+      onSubmitCard(selectedCard);
+
+      resetSelectedCard();
+    }
+  };
+
+  const handleInputValueChange = withDebounce<ChangeInputEventHandler>(
+    e => mutateSelectedCard(e.target.value),
+    500
+  );
+
   const isLoading = false;
 
   return (
-    <HStack as="form" w="100%" onSubmit={onSubmitCard}>
+    <HStack as="form" w="100%" onSubmit={handleSubmitCard}>
       <Menu>
         <VStack w="100%">
           <Input
@@ -32,19 +79,19 @@ export const SearchBox: FunctionComponent<SearchBoxProps> = ({
             size="lg"
             placeholder="Search card name..."
             mb="8px"
-            onChange={onChange}
+            onChange={handleInputValueChange}
           />
 
           <MenuButton w="100%" visibility="hidden" />
         </VStack>
 
-        {/* TODO: Options are not working properly. */}
+        {/* TODO: Options are not working properly.
         <MenuList w="100%" mt="-4">
           {possibleCards.length > 0 &&
             possibleCards.map((card, index) => (
               <MenuItem key={`possibleCards:${index}`}>{card}</MenuItem>
             ))}
-        </MenuList>
+        </MenuList> */}
       </Menu>
 
       <Button
