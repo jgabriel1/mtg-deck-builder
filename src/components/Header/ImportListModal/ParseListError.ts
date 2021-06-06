096a0a282253787a1edf2c682767134aead9b44f
jgabriel1 <josegabriel@det.ufc.br>
2021-06-06 19:53:26 -0300

    Created simple parse function to parse only the simpler format of list.

---
+++ src/components/Header/ImportListModal/ParseListError.ts
@@ -0,0 +1,1 @@
+export class ParseListError extends Error {}
---
+++ src/components/Header/ImportListModal/parseCardList.ts
@@ -0,0 +1,22 @@
+import { ParseListError } from './ParseListError';
+
+export const parseCardList = (listString: string) => {
+  try {
+    // Split lines on breaks
+    const lines = listString.split('\n');
+
+    // Separate quantity and card name
+    return lines.map(line => {
+      const firstWhitespaceIndex = line.indexOf(' ');
+      const quantity = Number(line.substr(0, firstWhitespaceIndex));
+      const cardName = line.substr(firstWhitespaceIndex + 1);
+
+      return {
+        quantity,
+        cardName,
+      };
+    });
+  } catch (err) {
+    throw new ParseListError(err);
+  }
+};
