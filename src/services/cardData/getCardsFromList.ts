919b131ea4489fda9ff3d67d3a3fb6a76027a012
jgabriel1 <josegabriel@det.ufc.br>
2021-06-14 19:39:27 -0300

    Modified getCardsFromList to be hard capped at 75 cards per request due to scryfall limitation.

--- src/services/cardData/getCardsFromList.ts
+++ src/services/cardData/getCardsFromList.ts
@@ -6,23 +6,50 @@ type CardsListResponse = {
   not_found: Array<{ name: string }>;
 };
 
+const splitArray = <T>(array: T[], sizeLimit: number) => {
+  const slices = [];
+
+  for (let i = 0; i <= array.length; i += sizeLimit)
+    slices.push(array.slice(i, i + sizeLimit));
+
+  return slices;
+};
+
+const CARD_LIMIT = 75;
+
 export const getCardsFromList = async (cardNames: string[]) => {
-  const { data: responseData } = await client.post<CardsListResponse>(
-    'cards/collection',
-    {
-      identifiers: cardNames.map(name => {
-        return { name };
-      }),
-    },
-    {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    }
+  const slices = splitArray(cardNames, CARD_LIMIT);
+
+  const results = await Promise.all(
+    slices.map(async slice => {
+      const { data: responseData } = await client.post<CardsListResponse>(
+        'cards/collection',
+        {
+          identifiers: slice.map(name => {
+            return { name };
+          }),
+        },
+        {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        }
+      );
+
+      return {
+        cards: responseData.data,
+        notFound: responseData.not_found,
+      };
+    })
   );
 
-  return {
-    cards: responseData.data,
-    notFound: responseData.not_found,
-  };
+  return results.reduce(
+    (accum, result) => {
+      return {
+        cards: [...accum.cards, ...result.cards],
+        notFound: [...accum.notFound, ...result.notFound],
+      };
+    },
+    { cards: [], notFound: [] }
+  );
 };
