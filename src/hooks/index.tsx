b7adb8392acf55667f901a79e0ee049056236e63
jgabriel1 <josegabriel@det.ufc.br>
2021-06-20 23:09:16 -0300

    Fixed provider.

--- src/hooks/index.tsx
+++ src/hooks/index.tsx
@@ -1,13 +1,11 @@
 import { FC } from 'react';
-
 import { DeckProvider } from './deck';
 import { ListOptionsProvider } from './listOptions';
 
 export const AppProvider: FC = ({ children }) => {
   return (
     <DeckProvider>
-      <ListOptionsProvider></ListOptionsProvider>
-      {children}
+      <ListOptionsProvider>{children}</ListOptionsProvider>
     </DeckProvider>
   );
 };
